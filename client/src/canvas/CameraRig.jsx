c9dd581ce83d430a5fc449be8061451bd42764d4
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-02-01 11:45:09 +0800

    shirt and positioning

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,16 @@
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import {easing} from 'maath';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+
+const CameraRig = ({ children}) => {
+    const group = useRef();
+const snap = useSnapshot(state);
+
+  return <group ref={group}>{children}</group>
+}
+
+export default CameraRig
