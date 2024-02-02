63bdc789d2eed80022526fa09b8d79f12f85549c
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-02-02 19:41:24 +0800

    small changes

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -39,6 +39,7 @@ const Shirt = () => {
                     rotation={[0,0,0]}
                     scale={0.15}
                     map={logoTexture}
+                    anisotropy={16}
                     />
                 )}
 
